4822bb45a9a3cbfe98cf0e013fbba0009a7bd967
Bijayaaaa <bijayabhandari2057@gmail.com>
2022-06-29 08:42:30 +0545

    allowed hosts update

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-l)(x)@9*xeqzcy%^_t)1p)so38ma8e$^8d(skqm2vdlf71ztlr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
